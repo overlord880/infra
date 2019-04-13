10172a8d221c50a4cab606ddc08a6f983989086e
Artem Polynkin <enultimo@yandex.ru>
2019-04-13 19:24:05 +0300

    creating instances with terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+variable app_disk_image {
+ description = "Disk image for reddit app"
+ default = "reddit-base-app"
+ }
+ variable db_disk_image {
+ description = "Disk image for reddit db"
+ default = "reddit-base-db"
+ }
+ variable public_key_path {
+ description = "Public ssh key"
+ default = "~/.ssh/id_ed25519.pub"
+ }
+ variable project {
+ description = "Project ID"
+ default = "infra-237414"
+}
+variable region {
+ description = "Region"
+ default = "europe-west1"
+}
